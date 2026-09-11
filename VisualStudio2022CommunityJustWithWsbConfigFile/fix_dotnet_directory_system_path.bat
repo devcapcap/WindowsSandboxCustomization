74e1824b64528a93b26617bb8d9f639f36c80403
devcapcap <35939873+devcapcap@users.noreply.github.com>
2026-09-11 19:11:48 +0200

    Update fix_dotnet_directory_system_path.bat

--- VisualStudio2022CommunityJustWithWsbConfigFile/fix_dotnet_directory_system_path.bat
+++ VisualStudio2022CommunityJustWithWsbConfigFile/fix_dotnet_directory_system_path.bat
@@ -5,16 +5,12 @@ set variables="C:\Program Files (x86)\dotnet;C:\Program Files\dotnet;C:\Program 
 for %%p in (%variables%) do (
     set "new_path=%%p"
     set "current_path=%PATH%"
-    :: Vérifier si le chemin est déjà dans PATH pour éviter les doublons
     echo %current_path% | findstr /i /c:"%new_path%" >nul
     if %errorlevel%==0 (
-        echo Le chemin %new_path% est déjà dans PATH.
+        echo The %new_path% already in the PATH.
     ) else (
-        :: Ajouter le nouveau chemin à PATH
         ::setx PATH "%current_path%;%new_path%" /M
-        echo Le chemin %new_path% a été ajouté à PATH.
-        
-        :: Mettre à jour PATH dans la session actuelle
+        echo The %new_path% added to PATH.
         ::set "PATH=%PATH%;%new_path%"
     )
 )
